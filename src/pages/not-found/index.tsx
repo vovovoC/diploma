70a9068c1084aec13acc28d4409f0843f3230c4b
Medeu-z <medeuovazhaniya01@gmail.com>
2023-04-01 17:50:43 +0600

    add 404 page

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import "./index.scss";
+import { useNavigate } from "react-router-dom";
+
+import logo from "../../app/assets/icons/logo-green.svg";
+import error from "../../app/assets/images/404.png";
 
 export function NotFoundPage() {
-  return <div className="">not found 404</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="not-found">
+      <img src={logo} alt="" className="logo show" />
+      <div className="body">
+        <div className="error-img">
+          <img src={error} alt=""/>
+          <p>404 <br /> not found</p>
+        </div>
+        <div className="error-text">
+           <p className="error-msg">Woops. Looks like this page doesn’t exist.</p>
+           <p className="back-link">back to <button  onClick={() => navigate("/")}>home</button></p>
+        </div>
+      </div>
+    </div>
+  );
 }
